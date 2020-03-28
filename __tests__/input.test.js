428872d46a3c0349a00b483ae2de5c1e61c3252c
Brett Packard <brettjayp@gmail.com>
2020-03-28 02:30:22 -0700

    notes class

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -5,22 +5,42 @@ const Input = require('../lib/input.js');
 // Bad input:
 // no command line data (null)
 const badInputA = [];
+const badArgsA = {};
 // command line input wrong (not the format of flag + data)
 const badInputB = ['WRONG'];
+const badArgsB = {
+  payload: 'WRONG'
+};
 // command line input has wrong flag
 const badInputC = ['-b', 'WRONG'];
+const badArgsC = {
+  flag: '-b',
+  payload: 'WRONG'
+};
 // data is not formatted properly (must be in a string format)
 const badInputD = ['-a', false];
+const badArgsD = {
+  flag: '-a',
+  payload: false
+};
 
 // Good input:
 // commane line input with '-a' followed by a string
 const goodInputA = ['-a', 'this is a note!'];
+const goodArgsA = {
+  flag: '-a',
+  payload: 'this is a note!'
+};
 
 describe('input module handles bad input gracefully', () => {
   it('handles empty input', () => {
     let result = new Input(badInputA);
     expect(result.valid()).toBeFalsy();
   });
+  // it('..handles empty input', () => {
+  //   let result = new Input(badArgsA);
+  //   expect(result.valid()).toBeFalsy();
+  // })
   it('handles wrong input', () => {
     let result = new Input(badInputB);
     expect(result.valid()).toBeFalsy();
--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -10,5 +10,25 @@ describe('High level description of tests contained', () => {
 
   it('Calls console.log as verified by sinon spy', () => {
     expect(spy.called).toBeTruthy();
+    spy.restore();
   });
 });
+
+describe('Testing good values', () => {
+  it('Is id a valid number', () => {
+    let result = new Notes(new Input(['-a', 'this is a note!']));
+    expect(result.validator.isNumber(result.id)).toBe(true);
+  });
+
+  it('Is payload a valid string', () => {
+    let result = new Notes(new Input(['-a', 'this is a note!']));
+    expect(result.validator.isString(result.note)).toBe(true);
+  });
+
+  it('Spy tells us we have console.log event', () => {
+    let spy = sinon.spy(console, 'log');
+    new Notes(new Input(['-a', 'this is a note!']));
+    expect(spy.called).toBeTruthy();
+    spy.restore();
+  })
+})
